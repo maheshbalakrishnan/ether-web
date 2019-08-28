8e5bbd76f91af0039fb06bbf1c760d224c831c0d
Mahesh Balakrishnan <maheshbalakrishnan90@gmail.com>
2019-08-27 21:31:09 -0700

    Adding Type script

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
     module: {
       rules: [
         {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
         },
         {
           test: /\.html$/,
@@ -34,6 +34,13 @@ module.exports = {
           },
         }
       ]
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
     },
     plugins: [
       new HtmlWebPackPlugin({
